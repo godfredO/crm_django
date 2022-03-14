784dd439bd824d399b31988f83eb711ca7c0581e
Godfred Owusu <godfredowusu@Godfreds-MacBook-Air.local>
2022-03-14 02:19:41 -0700

    initial

---
+++ leads/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from . import views
+
+app_name = 'leads'
+
+urlpatterns = [
+    path('', views.LeadListView.as_view(), name='list'),
+    path('<int:pk>/', views.LeadDetailView.as_view(), name='detail'), 
+    path('create/', views.LeadCreateView.as_view(), name='create'),
+    path('<int:pk>/update/', views.LeadUpdateView.as_view(), name='update'),
+    path('<int:pk>/delete/', views.LeadDeleteView.as_view(), name='delete'), 
+    path('<int:pk>/assign-agent/', views.AssignAgentView.as_view(), name='assign-agent'),
+    path('categories/', views.CategoryListView.as_view(), name= 'category-list'),
+    path('categories/<int:pk>/', views.CategoryDetailView.as_view(), name='category-detail'),
+    path('<int:pk>/category/', views.LeadCategoryUpdateView.as_view(), name='lead-category-update'),
+]
