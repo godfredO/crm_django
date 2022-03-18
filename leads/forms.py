a15cbbf3a3e98e4a867f078bc4bf23a91d265b67
Godfred Owusu <godfredowusu@Godfreds-MacBook-Air.local>
2022-03-18 12:30:13 -0700

    form validation

--- leads/forms.py
+++ leads/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from .models import Lead, Agent, Category
 from django.contrib.auth.forms import UserCreationForm, UsernameField
 from django.contrib.auth import get_user_model
-
+from django.core.exceptions import ValidationError
 
 
 User = get_user_model()
@@ -27,6 +27,22 @@ class LeadModelForm(forms.ModelForm):
             'phone_number',
             'email',
         )
+    
+    def clean_first_name(self):
+        pass
+        # data = self.cleaned_data['first_name']
+        # if data != "Joe":
+        #     raise ValidationError("Your namne is not Joe")
+        # return data
+
+    def clean(self):
+        pass
+        # first_name = self.cleaned_data['first_name']
+        # last_name = self.cleaned_data['last_name']
+        # if first_name +" "+ last_name != "Joe Soap":
+        #     raise ValidationError("Your name is not Joe Soap")
+        
+
 
 
 class LeadForm(forms.Form):
