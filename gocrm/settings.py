1438d784c1aac3094a570c178db50d7f78f00566
Godfred Owusu <godfredowusu@Godfreds-MacBook-Air.local>
2022-03-19 11:27:31 -0700

    leadmodelform profile_picture and enctype attribute of form tag

--- gocrm/settings.py
+++ gocrm/settings.py
@@ -192,3 +192,18 @@ if not DEBUG:
 
 
 
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'WARNING',
+    },
+}
+
--- leads/forms.py
+++ leads/forms.py
@@ -26,6 +26,7 @@ class LeadModelForm(forms.ModelForm):
             'description',
             'phone_number',
             'email',
+            'profile_picture'
         )
     
     def clean_first_name(self):
